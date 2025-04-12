47df2c7825f9941ec6ea4f9db62813e2f839135f
dkhmely <khmelnytskyi.denys@gmail.com>
2025-04-12 20:49:32 +0200

    test

--- IaC/kv.tf
+++ IaC/kv.tf
@@ -1,15 +1,15 @@
-# resource "azurerm_key_vault" "key_vault" {
-#   name                        = "${var.application}-${var.env}-kv"
-#   location                    = azurerm_resource_group.rg.location
-#   resource_group_name         = azurerm_resource_group.rg.name
-#   enabled_for_disk_encryption = true
-#   tenant_id                   = data.azurerm_client_config.current.tenant_id
-#   soft_delete_retention_days  = 7
-#   purge_protection_enabled    = false
-#   enable_rbac_authorization   = true
+resource "azurerm_key_vault" "key_vault" {
+  name                        = "${var.application}-${var.env}-kv"
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+  enable_rbac_authorization   = true
 
-#   sku_name = "standard"
-# }
+  sku_name = "standard"
+}
 
 # resource "azurerm_key_vault_secret" "secret" {
 #   name         = "${var.application}-${var.env}-sp"
