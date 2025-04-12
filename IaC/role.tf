a7f3ecd7b4736c0d3d3e529b170fa2d7eb379297
dkhmely <khmelnytskyi.denys@gmail.com>
2025-04-12 20:25:26 +0200

    fix

--- IaC/role.tf
+++ IaC/role.tf
@@ -4,8 +4,8 @@ resource "azurerm_role_assignment" "lz_spn_assignment" {
   principal_id         = azuread_service_principal.lz_app_sp.object_id
 }
 
-resource "azurerm_role_assignment" "kv_spn_assignment" {
-  scope                = azurerm_key_vault.key_vault.id
-  role_definition_name = "Key Vault Administrator"
-  principal_id         = azuread_service_principal.lz_app_sp.object_id
-}
+# resource "azurerm_role_assignment" "kv_spn_assignment" {
+#   scope                = azurerm_key_vault.key_vault.id
+#   role_definition_name = "Key Vault Administrator"
+#   principal_id         = azuread_service_principal.lz_app_sp.object_id
+# }
